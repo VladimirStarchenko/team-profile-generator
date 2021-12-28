43acccf54dab8a8a346fe325573909fea749c059
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2021-12-28 02:13:11 -0500

    enginner and intern html code written

--- src/page-template.js
+++ src/page-template.js
@@ -28,4 +28,39 @@ function htmlContent(data) {
   </div>
 `;
   }
+
+  function engineerCard(engineer) {
+    return `
+  <div class="card mr-1 mt-5 bg-light">
+    <div class="card-header bg-primary">
+      <h2 class="card-title text-light">${engineer.getName()}</h2>
+      <h3 class="card-title text-light bi bi-cup-fill">
+        Engineer <i class="fas fa-glasses"></i>
+      </h3>
+    </div>
+    <div class="card-body">
+      <p class="card">ID:${engineer.getId()}</p>
+      <p class="card">Email:${engineer.getEmail()}</p>
+      <p class="card">Github:${engineer.getGithub()}</p>
+    </div>
+  </div>`;
+  }
+
+  function internCard(intern) {
+    return `
+  <div class="card mr-1 mt-5 bg-light">
+    <div class="card-header bg-primary">
+      <h2 class="card-title text-light">${intern.getName()}</h2>
+      <h3 class="card-title text-light bi bi-cup-fill">
+        Intern <i class="fas fa-user-graduate"></i>
+      </h3>
+    </div>
+    <div class="card-body">
+      <p class="card">ID:${intern.getId()}</p>
+      <p class="card">Email:${intern.getEmail()}</p>
+      <p class="card">School:${intern.getSchool()}</p>
+    </div>
+  </div>`;
+  }
+  return cards;
 }
